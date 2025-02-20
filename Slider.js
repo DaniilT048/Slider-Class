6f94a9364da3e4e4473893222e8d5e920220c7f0
DaniilT048 <tis.daniil98@gmail.com>
2025-02-20 15:01:28 +0200

    making subscription for elements

--- Slider.js
+++ Slider.js
@@ -1,27 +1,60 @@
 export class Slider{
-    images = [];
+    sliderTimer = 2;
+
     intervalId = null;
-    currentSlide = 0;
     minDistanceForSwipe = 100;
     startTouchX = 0;
     endTouchX = 0;
-    sliderTimer = 2;
 
-    constructor(){
+
+    constructor(images){
+        if(!Array.isArray(images)||!images.length){
+            throw new Error("No images selected");
+        }
         this.images = images;
         this.currentSlide = images.length ;
-        this.intervalId = intervalId;
-        // this.minDistanceForSwipe = minDistanceForSwipe;
-        // this.endTouchX = endTouchX;
-        // this.startTouchX = startTouchX;
-        // this.sliderTimer = sliderTimer;
+
+        this.generateImage()
+        this.generateDots()
+
+        this.contentImg = document.querySelector("#slider .content");
+        this.imgSlide = document.querySelector("#slider img");
+
+        this.makeSubscriptionForElement()
+    }
+
+
+    makeSubscriptionForElement() {
+        document.querySelector("#slider .left").addEventListener("click", this.onLeft.bind(this));
+        document.querySelector("#slider .right").addEventListener("click", onRight);
+        document.querySelector("#slider .slider-navigation").addEventListener("click", onDotClick)
+        document.querySelector("#slider .start-sliding").addEventListener('click', startAutoSlides);
+        document.querySelector("#slider .stop-sliding").addEventListener('click', stopAutoSlides);
     }
 
     generateImage() {
         let imgHtml = '';
         this.images.forEach((image) => {
-            imgHtml += `<img src='img/${image}' alt="">`;
+            imgHtml += `<img src='${image}' alt="">`;
         })
         document.querySelector('#slider .content').innerHTML = imgHtml;
+    }
+
+    generateDots() {
+        let resultHtml = '';
+        this.images.forEach((image, index) => {
+            const activeClass = index === 0 ? "active" : "";
+            resultHtml += `<div class="dots ${activeClass}" data-dot="${index}"></div>`
+        })
+        document.querySelector('#slider .slider-navigation').innerHTML = resultHtml;
+    }
+
+    onLeft() {
+        currentSlide--;
+        if(currentSlide < 0){
+            currentSlide = images.length -1;
+        }
+        contentImg.style.transform = `translate(-${currentSlide * imgSlide.offsetWidth}px)`;
+        activeDot()
     }
 }
